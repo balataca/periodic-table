1cf6de7d9090b70d4c41668cc732fae45f8f4550
Augusto Zanoni <augustorz@tutanota.com>
2021-02-06 02:02:14 +0000

    fix: whitelist gitpod host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Whitelist host
+  config.hosts << "3000-teal-capybara-j7qfoj5p.ws-us03.gitpod.io"
 end
